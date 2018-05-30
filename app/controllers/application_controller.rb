d3aeea6c455b78e78b11abd0052b3eb2d7807c9b
Jayon Kim <jpkim921@gmail.com>
2018-05-30 23:15:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,9 @@ class ApplicationController < Sinatra::Base
 
   patch '/recipe/:id' do
     @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
   end
 
 
