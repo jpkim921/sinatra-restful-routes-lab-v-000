0c33ad197f9b15b010d206fc2790750177515188
Jayon Kim <jpkim921@gmail.com>
2018-05-30 23:49:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,6 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
-binding.pry
     redirect "/recipes/#{@recipe.id}"
   end
 
